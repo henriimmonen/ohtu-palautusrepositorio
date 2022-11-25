36e396f95e93e4705a5cc729cf23b3b253ff1279
Henri Immonen <henri.immonen000@gmail.com>
2022-11-25 09:49:59 +0200

    (5.10)

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -66,3 +66,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), 'Maito')
         self.assertEqual(ostos.lukumaara(), 1)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_on_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        piima = Tuote("Piimä", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(piima)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
